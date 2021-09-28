ea2fc2285c85ccff956a98b2756b46b33864be33
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2021-09-28 14:30:33 +0300

    Updates and added files

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-app.use(express.json())
 const morgan = require('morgan')
 const cors = require('cors')
-app.use(cors())
+const Person = require('./models/person')
+const person = require('./models/person')
 app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+
+// 3.13: puhelinluettelo ja tietokanta, step1 DONE
+
+// const password = process.argv[2]
+// const url = `mongodb+srv://fullstack:${password}@puhelinluettelo.kima0.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
 
 /*
 Tehtävä 3.7
@@ -32,6 +40,112 @@ app.use(morgan(function (tokens, request, response) {
   ].join(' ')
 }))
 
+app.get('/', (request, response) => {
+  response.send('<h1>Puhelinluettelo</h1>')
+})
+
+// Hae kontaktit
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error));
+})
+
+// Hae puhelinluettelon tiedot
+app.get('/info', (request, response, next) => {
+  const requestTime = new Date().toString()
+  
+  Person.countDocuments({}).then(persons => {
+    response.send(`<p>Puhelinluettelossa on tällä hetkellä ${persons} kontaktin tiedot</p> ${requestTime}`);
+  })
+  .catch(error => next(error));
+})
+
+// Hae kontaktin tiedot
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+// Poista kontakti
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// Lisää kontakti puhelinluetteloon
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+ /* if (body.name === undefined || body.phone === undefined) {
+    return response.status(400).json({ error: 'Nimi tai puhelinnumero puuttuu' })
+  }
+  */
+
+  const person = new Person({
+    name: body.name,
+    phone: body.phone,
+  })
+
+  person.save().then(savedPerson => savedPerson.toJSON())
+    .then(savedJSONFormattedPerson => {
+      response.json(savedJSONFormattedPerson)
+    }) 
+    .catch(error => next(error))
+})
+
+// Päivitä kontaktin puhelinnumero
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    phone: body.phone,
+  }
+  Person.findByIdAndUpdate(request.params.id, person)
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// Olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+/*
 let persons = [
   {
     id: 1,
@@ -54,20 +168,16 @@ let persons = [
     phone: "055-5566771"
   }
 ]
+*/
 
-app.get('/', (request, response) => {
-    response.send('<h1>Puhelinluettelo</h1>')
-})
-  
+/*
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+*/
 
-app.get('/info', (request, response) => {
-  const requestTime = new Date().toString()
-  response.send('<div><p>Puhelinluettelossa on tällä hetkellä ' + persons.length + ' kontaktin tiedot</p></div>' + requestTime)
-})
 
+/*
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
@@ -77,13 +187,41 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
 })
+*/
+/*
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+})
+*/
+/*
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
+*/
 
+
+/*
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
+*/
 
+
+/*
 app.post('/api/persons', (request, response) => {
   const person = {
     id: Math.floor(Math.random() * 1000),
@@ -102,8 +240,4 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(person)
   response.json(person)
 })
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+*/
---
+++ mongo.js
@@ -0,0 +1,49 @@
+
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('Give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@puhelinluettelo.kima0.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  phone: process.argv[4],
+})
+
+Person.find({}).then(result => {
+  if (person.name === undefined || person.phone === undefined) {
+    console.log('Phonebook:') 
+    result.forEach(person => {
+      console.log(person.name, person.phone)
+      mongoose.connection.close()
+    })
+  } else {
+    person.save({person}).then(persons => {
+      console.log(`added ${persons.name} number ${persons.phone} to phonebook`)
+      mongoose.connection.close()
+    })
+  }
+})
+
+
+
+ 
+
+
+ 
+
